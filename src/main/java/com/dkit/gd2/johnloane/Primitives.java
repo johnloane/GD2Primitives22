ea6628d73e96789dd1bad289d79a24b431af47ee
John Loane <john.loane@dkit.ie>
2021-09-21 12:27:04 +0100

    Showing GD2 22 how to use git
    Use your github account and the git remote to add your local
    commits to github

---
+++ src/main/java/com/dkit/gd2/johnloane/Primitives.java
@@ -0,0 +1,22 @@
+package com.dkit.gd2.johnloane;
+
+/**
+ * Hello world!
+ *
+ */
+public class Primitives
+{
+    public static void main(String[] args)
+    {
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println(myMaxIntValue);
+        System.out.println(myMinIntValue);
+
+        
+
+    }
+}
